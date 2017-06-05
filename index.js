d63fc1347e8f6b4d1097348b04021e4a6344283e
Connor Todd <todd37@purdue.edu>
2017-06-05 14:53:18 -0400

    Changing color of background and text

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 const personForm = document.querySelector('form#personForm')
 
-function handleSubmit(ev){
-    ev.preventDefault()
-    const f = ev.target
-    console.log(f.personName)
+function handleSubmit(ev) {
+  ev.preventDefault()
+  const f = ev.target
+  const name = f.personName.value
+
+  const a = ev.target
+  const home = a.homeTown.value
+
+  const b = ev.target
+  const color = b.color.value
+
+  const ret = 'Hello ' + name + ' from ' + home + ' '
+
+  document.querySelector('h1').textContent = ret
+
+  const fin = 'SUCCESS'
+
+  document.querySelector('h2').textContent = fin
+
+  var clr = 'yellow'
+
+  if(color === 'yellow'){
+        clr = 'green'
+  }
+  else{
+        clr = 'yellow'
+  }
+  
+  document.getElementById('myH2').style.color = clr
+  document.body.style.backgroundColor = color
 }
 
 personForm.addEventListener('submit', handleSubmit)
